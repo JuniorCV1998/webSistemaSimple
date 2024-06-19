6dd34b81076f6ba2658f24fe2935eb4e75cb2ab5
JuniorCV1998 <jcerronvicente@gmail.com>
2024-06-19 12:12:32 -0500

    proyecto 0

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ssimple.app',
+  appName: 'web-sistema-simple',
+  webDir: 'dist/web-sistema-simple/browser'
+};
+
+export default config;
---
+++ capacitor.sql
@@ -0,0 +1,23 @@
+ng serve -o
+--> Instala capacitor en Angular
+npm install @capacitor/core
+--> Extension de capacitor 'cli'
+npm install @capacitor/cli --save-dev
+--> Iniciar Capacitor
+npx cap init
+
+ng build
+---------------> dist/nameSistema <-----------------------
+
+--> Instalar capacitor Android & Ios
+npm install @capacitor/ios @capacitor/android
+
+npx cap sync ????
+
+--> Generar archivos Android & Compilar
+npx cap add android
+npx cap open android 
+
+--> Generar archivos Ios & Compilar
+npx cap add ios
+npx cap open ios
