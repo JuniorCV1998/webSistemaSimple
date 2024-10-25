906af63c801c043dfd36354e34541a1bcabffcb0
JuniorCV1998 <jcerronvicente@gmail.com>
2024-10-24 20:09:41 -0500

    update saldo pendiente a cobrar

--- src/app/core/appsettings.ts
+++ src/app/core/appsettings.ts
@@ -1,6 +1,6 @@
 export const appsettings = {
-    API_SERVER: "http://localhost:8080/",
-    //API_SERVER: "https://apisistemasimple-production.up.railway.app/", // raiwall
+    //API_SERVER: "http://localhost:8080/",
+    API_SERVER: "https://apisistemasimple-production.up.railway.app/", // raiwall
     API_COMP_INVERSION: "api/inversion/",       //inverios
     API_COMP_INVERSORES: "api/inversionista/",  //inversiones
     API_COMP_REGISTER: "register/",  //inversiones
