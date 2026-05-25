654c9ca2f47114c62e4db5ebd3747472694f9d82
JuniorCV1998 <jcerronvicente@gmail.com>
2026-05-25 08:58:08 -0500

    new version app android others accounts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ssimple.app',
+  appId: 'com.ssimplepay.app',
   appName: 'web-sistema-simple',
   webDir: 'dist/web-sistema-simple/browser',
   plugins: {
